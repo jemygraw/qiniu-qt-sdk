1248e39d5d5d1d72c339433e4d935e3ede2ac7c5
Jemy <jemygraw@gmail.com>
2014-10-16 09:23:28 +0800

    Add base64 function test.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "ui/QNMainWindow.h"
 #include <QApplication>
-
+#include <QtTest/QtTest>
+#include "test/Base64Test.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QNMainWindow w;
     w.show();
-
+    Base64Test t;
+    QTest::qExec(&t);
     return a.exec();
 }
--- test/Base64Test.cpp
+++ test/Base64Test.cpp
@@ -1,12 +1,15 @@
 #include "test/Base64Test.h"
 #include "qiniu/QNUtils.h"
 #include <QByteArray>
+
 void Base64Test::base64EncodeTest()
 {
     QByteArray data("hello");
     QString encoded = QNUtils::urlSafeBase64Encode(data);
     QVERIFY(encoded == "aGVsbG8=");
+    QByteArray decoded = QNUtils::urlSafeBase64Decode(encoded);
+    QVERIFY(decoded == data);
 }
 
-QTEST_MAIN(Base64Test)
+
 
