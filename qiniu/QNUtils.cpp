6388b30f2ff058696104b79c2c0e140c38baa20d
Jemy <jemygraw@gmail.com>
2014-10-28 10:00:53 +0800

    Add QNUtils class, which contains some helper methods.

--- qiniu/QNUtils.cpp
+++ qiniu/QNUtils.cpp
@@ -1,19 +1,74 @@
 #include "QNUtils.h"
 #include <QByteArray>
 #include <QString>
+#include <QCryptographicHash>
+#include <QUrl>
+
+
+const char QNUtils::KEY_ESCAPE_UNRESERVE_BYTES[] = {'!', '*', '\'', '(', ')', ';', ':', '@',
+                                                    '&', '=', '+', '$', ',', '/', '#', '[', ']' };
+
+const char QNUtils::KEY_ESCAPE_RESERVE_BYTES[] = {'?'};
 
 QNUtils::QNUtils()
 {
 }
 
+QNUtils::~QNUtils()
+{
+
+}
+
+// Url safe base64 encode
 QString QNUtils::urlSafeBase64Encode(QByteArray data)
 {
     QByteArray encodedData = data.toBase64(QByteArray::Base64UrlEncoding);
     return QString(encodedData);
 }
 
+// Url safe base64 decode
 QByteArray QNUtils::urlSafeBase64Decode(QString data)
 {
     QByteArray bytesToDecode=QByteArray(data.toLocal8Bit());
     return QByteArray::fromBase64(bytesToDecode);
+}
+
+// Sha1 hash
+QByteArray QNUtils::sha1(QByteArray data)
+{
+    QCryptographicHash sha1Hash(QCryptographicHash::Sha1);
+    sha1Hash.addData(data);
+    return sha1Hash.result();
+}
+
+// Hmac sha1 hash using secret key
+QByteArray QNUtils::hmacSha1(QByteArray data, QByteArray secretKey)
+{
+    int blockSize = 64;
+    if (secretKey.length() > blockSize) {
+        secretKey = QCryptographicHash::hash(secretKey, QCryptographicHash::Sha1);
+    }
+
+    QByteArray innerPadding(blockSize, char(0x36));
+    QByteArray outerPadding(blockSize, char(0x5c));
+
+    for (int i = 0; i < secretKey.length(); i++) {
+        innerPadding[i] = innerPadding[i] ^ secretKey.at(i);
+        outerPadding[i] = outerPadding[i] ^ secretKey.at(i);
+    }
+
+    QByteArray total = outerPadding;
+    QByteArray part = innerPadding;
+    part.append(data);
+    total.append(QCryptographicHash::hash(part, QCryptographicHash::Sha1));
+    QByteArray hashed = QCryptographicHash::hash(total, QCryptographicHash::Sha1);
+    return hashed;
+}
+
+// Url escaped key
+// @see http://kb.qiniu.com/52slk76w
+// @see http://en.wikipedia.org/wiki/Percent-encoding
+QString QNUtils::escapeKey(QString key)
+{
+    return QUrl::toPercentEncoding(key,QNUtils::KEY_ESCAPE_UNRESERVE_BYTES,QNUtils::KEY_ESCAPE_RESERVE_BYTES);
 }
--- qiniu/QNUtils.h
+++ qiniu/QNUtils.h
@@ -8,8 +8,14 @@ class QNUtils
 {
 public:
     QNUtils();
+    ~QNUtils();
+    static const char KEY_ESCAPE_RESERVE_BYTES[];
+    static const char KEY_ESCAPE_UNRESERVE_BYTES[];
     static QString urlSafeBase64Encode(QByteArray data);
     static QByteArray urlSafeBase64Decode(QString data);
+    static QByteArray sha1(QByteArray data);
+    static QByteArray hmacSha1(QByteArray data, QByteArray secretKey);
+    static QString escapeKey(QString key);
 };
 
 #endif // QNUTILS_H
