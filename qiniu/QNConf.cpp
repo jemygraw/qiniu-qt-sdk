4682c9bd839363d61b0d8c6edc7d754f4b277061
Jemy <jemygraw@gmail.com>
2014-10-28 10:07:40 +0800

    Add qiniu constants.

--- qiniu/QNConf.cpp
+++ qiniu/QNConf.cpp
@@ -1,5 +1,27 @@
 #include "QNConf.h"
+#include <QString>
+
+// Upload & Resource Management Host
+const QString QNConf::UPLOAD_HOST = QString("http://upload.qiniu.com");
+const QString QNConf::RS_HOST = QString("http://rs.qbox.me");
+const QString QNConf::RSF_HOST = QString("http://rsf.qbox.me");
+const QString QNConf::IOVIP_HOST = QString("http://iovip.qbox.me");
+
+//  Access Key & Secret Key
+const QString QNConf::ACCESS_KEY = QString("<Your Access Key>");
+const QByteArray QNConf::SECRET_KEY = QByteArray("<Your Secret Key>");
+
+// Style Delimiter
+const QString QNConf::DEFAULT_STYLE_DELIMITER = QString("-");
+
+// Version
+const QString QNConf::VERSION = QString("1.0.0");
 
 QNConf::QNConf()
 {
 }
+
+//std::cout<< QNConf::UPLOAD_HOST.toStdString()<<std::endl;
+//std::cout<< QNConf::RS_HOST.toStdString()<<std::endl;
+//std::cout<< QNConf::RSF_HOST.toStdString()<<std::endl;
+//std::cout<< QNConf::IOVIP_HOST.toStdString()<<std::endl;
--- qiniu/QNConf.h
+++ qiniu/QNConf.h
@@ -1,9 +1,24 @@
 #ifndef QNCONF_H
 #define QNCONF_H
 
+class QString;
+class QByteArray;
+
 class QNConf
 {
 public:
+    static const QString UPLOAD_HOST;
+    static const QString RS_HOST;
+    static const QString RSF_HOST;
+    static const QString IOVIP_HOST;
+
+    static const QString ACCESS_KEY;
+    static const QByteArray SECRET_KEY;
+
+    static const QString DEFAULT_STYLE_DELIMITER;
+
+    static const QString VERSION;
+
     QNConf();
 };
 
